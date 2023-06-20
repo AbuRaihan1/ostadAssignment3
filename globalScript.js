ba52a8c7a38df1b8da48f86b20b7f61d4aee7dcc
Abu Raihan <raihanahmed01973@gmail.com>
2023-06-20 22:15:52 +0600

    form submitted done question nine

--- globalScript.js
+++ globalScript.js
@@ -1,5 +1,46 @@
-// change image function start
+// form submiting problem solving start, problem 9
 
+const submitFormButton = document.getElementById("submitFormButton");
+
+function submitFormHandler(e) {
+  e.preventDefault();
+  const mainForm = document.getElementById("form");
+  const form = mainForm;
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const confirmPassword = form.confirmPassword.value;
+
+  if (!name || !email || !password || !confirmPassword) {
+    return alert("fill in the all inputs");
+  }
+
+  const checkEmailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!checkEmailValidate.test(email)) {
+    return alert("enter a valid email address");
+  }
+
+  const checkPasswordValidate =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  if (!checkPasswordValidate.test(password)) {
+    return alert(
+      "Password should have a minimum length of 8 characters and contain at least one uppercase letter, one lowercase letter, and one digit."
+    );
+  }
+
+  if (password !== confirmPassword) {
+    return alert("Passwords do not match");
+  }
+
+  alert("Form is submitted successfully");
+  console.log(name, email, password, confirmPassword);
+  form.reset();
+}
+
+submitFormButton.addEventListener("click", submitFormHandler);
+// form submiting problem solving end, problem 9
+
+// change image function start
 const changeImageButton = document.getElementById("changeImgButton");
 let currentImage = 1;
 changeImageButton.addEventListener("click", function () {
@@ -14,7 +55,6 @@ changeImageButton.addEventListener("click", function () {
     currentImage = 0;
   }
 });
-
 // change image function end
 
 // add to list function start
@@ -36,7 +76,6 @@ addToListButton.addEventListener("click", function () {
 // add to list function end
 
 // change p element background by click start : que no 10;
-
 const changeBgTextButton = document.getElementById("changeBgTextButton");
 const changeBgYellow = document.getElementById("changeBgYellow");
 changeBgTextButton.addEventListener("click", function () {
